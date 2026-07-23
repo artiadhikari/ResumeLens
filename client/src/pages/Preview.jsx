384ee2ae4955fe9401c568db004274ccdd5be6cb
Arti Adhikari <adhikariarti7@gmail.com>
2026-07-23 22:50:39 +0530

    fixed frontend bug

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
-import ResumePreview from '../components/ResumePreview'
-import Loader from '../components/Loader'
+import ResumePreview from '../Components/ResumePreview'
+import Loader from '../Components/Loader'
 import { ArrowLeftIcon } from 'lucide-react'
 import api from '../configs/api'
 
